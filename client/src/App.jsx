87e35bff5e990778bdfd64c5c56242e3c1a6cc72
Bastien Daniel <bastien.daniel@rawfish.com>
2023-10-02 15:22:13 +0200

    second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-import PaginationTest1 from "./components/PaginationTest1";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PaginationTest2 from "./components/PaginationTest2";
 
